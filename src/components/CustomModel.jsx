a2334f6c4b9e29ec7c4ba0247efd3da7a7749438
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-19 18:08:13 +0530

    adding popoup compo with custom css file

---
+++ src/components/CustomModel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './CustomModel.css'
+function CustomModel() {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default CustomModel;
--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { showUser } from "../features/userDetailSlice";
+import CustomModel from "./CustomModel";
 
 function Read() {
   const dispatch = useDispatch();
@@ -13,33 +14,34 @@ function Read() {
   }
   return (
     <>
-  <h2 className="text-center text-info fw-bold p-2">All Data</h2>
-  <div className="cards-wrapper d-flex flex-row flex-wrap justify-content-center gap-3">
-    {users &&
-      users.map((ele) => (
-        <div key={ele.id} className="card w-25 bg-primary-subtle">
-          <div className="card-body d-flex flex-column align-items-center justify-content-center">
-            <h5 className="card-title">{ele.name}</h5>
-            <h6 className="card-subtitle mb-2 text-body-secondary">
-              {ele.email}
-            </h6>
-            <p className="card-text">{ele.gender}</p>
-            <div>
-              <a href="#" className="card-link">
-                View
-              </a>
-              <a href="#" className="card-link">
-                Edit
-              </a>
-              <a href="#" className="card-link">
-                Delete
-              </a>
+      <CustomModel />
+      <h2 className="text-center text-info fw-bold p-2">All Data</h2>
+      <div className="cards-wrapper d-flex flex-row flex-wrap justify-content-center gap-3">
+        {users &&
+          users.map((ele) => (
+            <div key={ele.id} className="card w-25 bg-primary-subtle">
+              <div className="card-body d-flex flex-column align-items-center justify-content-center">
+                <h5 className="card-title">{ele.name}</h5>
+                <h6 className="card-subtitle mb-2 text-body-secondary">
+                  {ele.email}
+                </h6>
+                <p className="card-text">{ele.gender}</p>
+                <div>
+                  <a href="#" className="card-link">
+                    View
+                  </a>
+                  <a href="#" className="card-link">
+                    Edit
+                  </a>
+                  <a href="#" className="card-link">
+                    Delete
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
-  </div>
-</>
+          ))}
+      </div>
+    </>
   );
 }
 
