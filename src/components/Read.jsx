85c1d4fe3dadca4a5d0a623618170128ffe5c405
ritikmaurya4747 <ritikmaurya4747@gmail.com>
2024-10-21 17:45:34 +0530

    after searching the data and data found and all crud are done

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 function Read() {
   const dispatch = useDispatch();
   const [id, setId] = useState();
+  const [radioData, setRadioData] = useState("");
   const [showPopup, setShowPopup] = useState(false);
   const { users, loading, searchData } = useSelector((state) => state.app);
   useEffect(() => {
@@ -25,6 +26,32 @@ function Read() {
         />
       )}
       <h2 className="text-center  fw-bold p-2">All Data</h2>
+      <div className="d-flex justify-content-center align-items-center gap-2 pb-3">
+        <input
+          className="form-check-input"
+          name="gender"
+          type="radio"
+          checked={radioData === ""}
+          onChange={(e) => setRadioData("")}
+        />
+        <label className="form-check-label text-primary fs-4">All</label>
+        <input
+          className="form-check-input"
+          name="gender"
+          value="Male"
+          type="radio"
+          onChange={(e) => setRadioData(e.target.value)}
+        />
+        <label className="form-check-label text-primary fs-4">Male</label>
+        <input
+          className="form-check-input"
+          name="gender"
+          value="Female"
+          type="radio"
+          onChange={(e) => setRadioData(e.target.value)}
+        />
+        <label className="form-check-label text-primary fs-4">Female</label>
+      </div>
       <div className="cards-wrapper d-flex flex-row flex-wrap justify-content-center gap-3">
         {users &&
           users
@@ -35,6 +62,14 @@ function Read() {
                 return ele.name
                   .toLowerCase()
                   .includes(searchData.toLowerCase());
+              }
+            }).filter((ele) => {
+              if(radioData === "Male" ){
+                return ele.gender === radioData;
+              } else if(radioData === "Female" ){
+                return ele.gender === radioData;
+              }else{
+                return ele;
               }
             })
             .map((ele) => (
